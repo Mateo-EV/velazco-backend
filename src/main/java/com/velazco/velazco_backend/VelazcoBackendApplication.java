8ab820610bfc779427f33af2c5bdea4aff1f50b3
Mateo-EV <riojamatthew@gmail.com>
2025-04-09 20:01:40 -0500

    start

---
+++ src/main/java/com/velazco/velazco_backend/VelazcoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.velazco.velazco_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelazcoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VelazcoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/velazco/velazco_backend/VelazcoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velazco.velazco_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelazcoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
