6c66a7b837116b5b97098903a4bddf749e563da6
Mateo-EV <riojamatthew@gmail.com>
2025-07-02 22:10:54 -0500

    fix: add clock provider

--- src/main/java/com/velazco/velazco_backend/config/TimeZoneConfig.java
+++ src/main/java/com/velazco/velazco_backend/config/TimeZoneConfig.java
@@ -6,11 +6,18 @@ import java.time.ZoneId;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import jakarta.validation.ClockProvider;
+
 @Configuration
 public class TimeZoneConfig {
 
   @Bean
   Clock systemClock() {
     return Clock.system(ZoneId.of("America/Lima"));
+  }
+
+  @Bean
+  ClockProvider clockProvider(Clock clock) {
+    return () -> clock;
   }
 }
