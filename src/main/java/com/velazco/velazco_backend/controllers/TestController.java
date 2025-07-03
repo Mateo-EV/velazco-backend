71f87f09331a7abd65f7edb0b94540aeae206386
Mateo-EV <riojamatthew@gmail.com>
2025-07-02 22:18:34 -0500

    test: clock route

--- src/main/java/com/velazco/velazco_backend/controllers/TestController.java
+++ src/main/java/com/velazco/velazco_backend/controllers/TestController.java
@@ -1,12 +1,27 @@
 package com.velazco.velazco_backend.controllers;
 
+import java.time.Clock;
+import java.time.LocalDate;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 public class TestController {
+
+  private final Clock clock;
+
   @GetMapping("/")
   public String hello() {
     return "Hello world from Java Spring Boot!";
+  }
+
+  @GetMapping("/clock")
+  public String getDateNow() {
+    LocalDate today = LocalDate.now(clock);
+    return "Hoy en Perú: " + today;
   }
 }
